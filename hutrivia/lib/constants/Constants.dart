5e8788a3f0649085246ff5e686363fca8420a6e0
PutriLauraa <rahmaputrilaura@gmail.com>
2023-08-26 15:04:42 +0700

    Added assets

---
+++ hutrivia/lib/constants/Constants.dart
@@ -0,0 +1,16 @@
+// Storing colors and stuff here
+
+import 'package:flutter/material.dart';
+
+// color for correct answer
+const correct = Colors.green;
+
+// color for incorrect answer
+const incorrect = Colors.red;
+
+// neutral color
+const neutral = Colors.white;
+
+// set some background color
+const Color background = Colors.blue;
+
